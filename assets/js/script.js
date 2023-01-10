08ee98e2b0103420d5bab19058a5a47889cb29eb
Jerry <jamesharold1738@gmail.com>
2023-01-10 15:41:12 -0500

    added new code to script.js for dictionary api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,40 @@
 var searchbar = document.querySelector(".input-group-field");
 var button = document.querySelector(".button");
+var dictionarytext = $('#dictionaryTextArea');
 
 
 function Definition(){
+    $('#dictionaryTextArea').empty()
  var url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + searchbar.value
-fetch(url).then(response =>(response.json())).then(data=> {console.log(data[0])
-     console.log(data.values())
+fetch(url).then(response =>(response.json())).then(data=> {
+      localStorage.setItem("Definition", searchbar.value);
+      var definition = data[0].meanings
+      $("#dictionaryTextArea").append(origin)
+
+
+     for(const ele of definition){
+       // var loopDefinition =  data[0].meanings[i].definitions[i].definition;
+        //$("#dictionaryTextArea").append(loopDefinition); 
+        //console.log(ele)
+        var part = ele.partOfSpeech || ""  
+        var definitionPart = ele.definitions
+    
+     ele.definitions.forEach(definition => {
+        console.log(definition)
+        console.log(definitionPart[0].definition)
+       $("#dictionaryTextArea").append(definition , part)
+       $("#dictionaryTextArea").append(definition , definitionPart[0].definition)
+    
+      })
+     
+  // in for loop grab part 
+  //do another for loop using ele to update array
+  // do html stuff 
+}
+     
+     
+
+     
 }
 );
 }
