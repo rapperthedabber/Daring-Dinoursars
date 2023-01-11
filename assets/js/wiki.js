9c68975eb39f917cefbf79c1c16c64c6741fae1e
mtanng9 <mlt152498@gmail.com>
2023-01-11 00:57:02 -0500

    got dynamic search working with one word terms and basic html on page

--- assets/js/wiki.js
+++ assets/js/wiki.js
@@ -8,7 +8,7 @@ function fetchWikipedia(searchTerm) {
     var params = {
         action: "opensearch",
         search: searchTerm,
-        limit: "5",
+        limit: "1",
         namespace: "0",
         format: "json"
     };
@@ -18,15 +18,45 @@ function fetchWikipedia(searchTerm) {
 
     fetch(url)
         .then(function(response){return response.json();})
-        .then(function(response) {console.log(response);})
-        .catch(function(error){console.log(error);});
+        .then(function(response) {$('#wiki-title').text(response[1][0]);})
+        .catch(function(error){$('#wiki-article').text(error);});
+}
 
+function fetchWikipediaBody(searchTerm) { 
+
+    var url = "https://en.wikipedia.org/w/api.php"; 
+
+    var params = {
+        action: "parse",
+        page: searchTerm,
+        prop: "text",
+        formatversion: 2,
+        format: "json"
+    };
+
+    url = url + "?origin=*";
+    Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+
+    fetch(url)
+        .then(function(response){return response.json();})
+        .then(function(response){$('#wiki-article').text(response.parse.text);})
+        //.then(function(parse) {$('#wiki-article').text(parse);})
+        .catch(function(error){console.log(error);});
+}
+
+function callWikiAPIs() {
+    var search = $('#search').val();
+    fetchWikipedia(search);
+    fetchWikipediaBody(search);
 }
 
 $('#searchBtn').click(function(event) {
     event.preventDefault();
-
-    var search = $('#search').val();
-    console.log(search);
-    fetchWikipedia(search);
+    callWikiAPIs();
 })
+
+searchbar.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        callWikiAPIs();
+    }
+});
