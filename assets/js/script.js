f8f2e317bf49857fd9f492173fee871c7c479a5e
ELENA VALEEVA <elenavaleeva05@gmail.com>
2023-01-11 21:02:06 -0500

    youTube for resolving issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,6 @@
 var searchbar = document.querySelector(".input-group-field");
 var button = document.querySelector(".button");
 var dictionarytext = $('#dictionaryTextArea');
-
-
 function Definition() {
     $('#dictionaryTextArea').empty()
     var url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + searchbar.value
@@ -12,13 +10,10 @@ function Definition() {
         var definition = data[0].meanings
         var phonetics = data[0].phonetics[1].text
         var sound = data[0].phonetics[1].audio
-
         $("#dictionaryTextArea").append()
-
-
         for (const ele of definition) {
             // var loopDefinition =  data[0].meanings[i].definitions[i].definition;
-            //$("#dictionaryTextArea").append(loopDefinition); 
+            //$("#dictionaryTextArea").append(loopDefinition);
             //console.log(ele)
             var part = ele.partOfSpeech || ""
             var definitionPart = ele.definitions
@@ -28,32 +23,21 @@ function Definition() {
                 window.location.href = sound;
             };
             $("#dictionaryTextArea").append(img)
-
             ele.definitions.forEach(definition => {
                 console.log(definition)
                 console.log(definitionPart[0].definition)
-
-
                 // $("#dictionaryTextArea").append(definition , part)
-
                 $("#dictionaryTextArea").append(phonetics, part, definitionPart[0].definition)
                 //var img = $("#audio-image").setAttribute("src","https://png.pngtree.com/png-vector/20190307/ourmid/pngtree-vector-high-volume-icon-png-image_762948.jpg")
             }
             )
-
-
-            // in for loop grab part 
+            // in for loop grab part
             //do another for loop using ele to update array
-            // do html stuff 
+            // do html stuff
         }
-
-
-
-
     }
     );
 }
-
 button.addEventListener("click", Definition);
 searchbar.addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
