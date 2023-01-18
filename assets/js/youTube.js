35176205fc033745addce52b8ebc99f4d859d5d2
alexander1262 <106128188+alexander1262@users.noreply.github.com>
2023-01-18 18:31:00 -0500

    search history working

--- assets/js/youTube.js
+++ assets/js/youTube.js
@@ -1,11 +1,5 @@
-
-
-
 $(document).ready(function() {
   var apiKey = 'AIzaSyA0_825U4h0ZZTExYZ_6QZfquUkOE2gf1Y'  
-//   var video = ''
-//   var videos = $('#videos')
-
 
 
  $('#searchBtn').click(function(event) {
@@ -13,17 +7,15 @@ $(document).ready(function() {
 
     var search = $('#search').val()
     console.log(search)
-   videoSearch(apiKey, search, 5)
+   videoSearch(search)
 })
 
 
-
-
- function videoSearch(key, search, maxResults) {
-    
+function videoSearch(search) {
+    var maxResults = 5
     $('#youtubeVideoTitle').text('Youtube Results:')
     $("#videos").empty()
-    let url = `https://www.googleapis.com/youtube/v3/search?key=${key}&type=video&part=snippet&maxResults=${maxResults}&q=${search}`;
+    let url = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&type=video&part=snippet&maxResults=${maxResults}&q=${search}`;
 
     fetch(url).then(res => res.json()).then(data => {
 
@@ -41,18 +33,9 @@ $(document).ready(function() {
         console.log( $("#videos"))
         $("#videos").append(videoAnchorTag.append(imgEl))
 
-        
-    //     video = `
-    //    <iframe width="420" height"315 src="http://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe> 
-        
-    //     `
-    //     console.log(video)
-    //    $("#videos").append(video)
     });
    
 })
-
-
  }
 })
 
